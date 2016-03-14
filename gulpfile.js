5783ecf69d4903eb4bf1687476e5782971185395
Jack Liou <jkliou@gmail.com>
2016-03-14 22:13:09 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var gutil = require('gulp-util');
+var browserSync = require('browser-sync').create();
+var path = require('path');
+var collapse = require('bundle-collapser/plugin');
+
+//process.env.BROWSERIFYSHIM_DIAGNOSTICS=1;
+
+gulp.task('js:videojs', function () {
+    return browserify()
+        .require(path.dirname(require.resolve('video.js')) + "/video.dev.js", {expose: 'video.js', fullPaths: false})
+        .plugin(collapse)
+        .bundle()
+        .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+        .pipe(source('videojs.js'))
+        .pipe(gulp.dest('./dist/js'));
+});
+gulp.task('js:videojs-hls-standalone', function () {
+    return browserify('./src/videojs.hls/index.js', { fullPaths: false })
+        .exclude('video.js')
+        .plugin(collapse)
+        .bundle()
+        .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+        .pipe(source('videojs.hls.js'))
+        .pipe(gulp.dest('./dist/js'));
+});
+gulp.task('js:videojs-ga-standalone', function() {
+    return browserify('./src/videojs.ga/index.js', { fullPaths: false })
+        .exclude('video.js')
+        .plugin(collapse)
+        .bundle()
+        .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+        .pipe(source('videojs.ga.js'))
+        .pipe(gulp.dest('./dist/js'));
+});
+gulp.task('js:videojs-bundle', function () {
+    return browserify('./src/videojs.bundle/index.js', { fullPaths: false })
+        .plugin(collapse)
+        .bundle()
+        .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+        .pipe(source('videojs.bundle.js'))
+        .pipe(gulp.dest('./dist/js'));
+});
+gulp.task('js:copy-videojs-vtt.js', function () {
+    return gulp.src(path.dirname(require.resolve('video.js')) + '/../../node_modules/vtt.js/dist/vtt.min.js')
+        .pipe(gulp.dest('dist/js'));
+});
+gulp.task('js:copy-videojs-srt.js', function () {
+    return gulp.src(path.dirname(require.resolve('video.js')) + '/../../src/videosub-0.9.9.js')
+        .pipe(gulp.dest('dist/js'));
+});
+gulp.task('js', [
+    'js:videojs',
+    'js:videojs-hls-standalone',
+    'js:videojs-ga-standalone',
+    'js:videojs-bundle'
+    //,'js:copy-videojs-vtt.js'
+    //,'js:copy-videojs-srt.js'
+]);
+
+gulp.task('css:copy-videojs.min.css', function () {
+    gulp.src(path.dirname(require.resolve('video.js')) + '/video-js.min.css')
+        .pipe(gulp.dest('dist/css'));
+});
+gulp.task('css', [
+    'css:copy-videojs.min.css'   
+]);
+
+gulp.task('fonts:copy-videojs-font', function () {
+    gulp.src(path.dirname(require.resolve('video.js')) + '/font/**')
+        .pipe(gulp.dest('dist/css/font'));
+});
+gulp.task('fonts', ['fonts:copy-videojs-font']);
+
+gulp.task('swf:copy-videojs-swf', function () {
+    gulp.src('node_modules/videojs-swf/dist/*.swf')
+        .pipe(gulp.dest('dist/swf'));
+});
+gulp.task('swf', ['swf:copy-videojs-swf']);
+
+gulp.task('dev:bs', ['default'], function() {
+    browserSync.init({
+        server: {baseDir: './'},
+        directory: true
+    });
+
+    gulp.watch(['src/**/*.js', 'examples/*.html'], ['dev:watch']);
+});
+gulp.task('dev:watch', ['default'], browserSync.reload);
+gulp.task('dev', ['dev:bs']);
+
+
+gulp.task('default', ['js', 'fonts', 'css', 'swf']);
---
+++ src/videojs.bundle/index.js
@@ -0,0 +1,37 @@
+var vtt = require('vtt.js');
+global.WebVTT = vtt.WebVTT;
+global.VTTCue = vtt.VTTCue;
+global.VTTRegion = vtt.VTTRegion;
+
+//var ender = require('ender');
+//global.$ = require('ender');
+//var srt = require('videosub');  // srt.js
+global.pkcs7 = require('pkcs7'); // required for hls plugin
+global.videojs = require('videojs/video-js');
+//global.Hls = require('hls.js/src/hls.js');
+global.Hls = require('hls.js');
+//global.Hls = require('hls.js/src/hls').default;
+
+// persistent volume
+require('videojs-persistvolume');
+// Google Analytics
+require('videojs-ga');
+// hls playback
+require('contrib/media-sources');
+require('contrib/hls/videojs-hls');
+require('contrib/hls/stream');
+require('contrib/hls/flv-tag');
+require('contrib/hls/exp-golomb');
+require('contrib/hls/h264-extradata');
+require('contrib/hls/h264-stream');
+require('contrib/hls/aac-stream');
+require('contrib/hls/metadata-stream');
+require('contrib/hls/segment-parser');
+require('contrib/hls/m3u8/m3u8-parser');
+require('contrib/hls/xhr');
+require('contrib/hls/playlist');
+require('contrib/hls/playlist-loader');
+require('contrib/hls/decrypter');
+require('videojs/videojs-hlsjs.js');
+
+
